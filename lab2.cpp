3c2cdfcc3d49b944dfc99892d1bf14aa0a3ba45a
Andrey Rusakov <71116358+sLNbk3y@users.noreply.github.com>
2020-09-26 05:42:29 +0300

    Add files via upload
    Добавил 2 лабу, без 3 задания

---
+++ lab2.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+/*
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	using namespace std;
+	int UInput, zer, numb, i_2, i_8;
+	UInput = zer = numb = i_2 = i_8 = 0;
+	cout << "Введите число" << endl;
+	cin >> UInput;
+	while (UInput>=zer)
+	{
+		if (zer % 2 == 0) {
+			cout << zer << endl;
+			numb++;
+			i_2 = i_2 + zer;
+		}
+		zer++;
+	}
+	zer = 0;
+	while (UInput >= zer) {
+		if (zer % 8 == 0) {
+			cout << zer << endl;
+			i_8 = i_8 + zer;
+		}
+		zer++;
+	}
+	cout << "Чисел кратных 2 = " << numb << endl;
+	cout << "Сумма кратных 2 = " << i_2 << endl;
+	cout << "Сумма кратных 8 = " << i_8 << endl;
+}
+*/
+
